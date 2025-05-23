b65cd494f38d8c031f1923b3dab4a5e123239f41
Raman Varma <ramanvarma@berkeley.edu>
2025-05-23 17:05:07 +0400

    Remove redundant 'existing secret' logic for getting the onboarding access token

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"log"
-	"os"
 
 	"operator/pkg/client"
 
@@ -11,7 +10,7 @@ import (
 )
 
 func main() {
-	log.Println("Starting auto-np client...")
+	log.Println("Starting AutoNP Operator...")
 
 	logger, err := zap.NewProduction()
 	if err != nil {
@@ -21,29 +20,20 @@ func main() {
 	// Create a context with cancel
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	// Load configuration from environment variables (populated by Helm)
-	config := client.LoadConfigFromEnv()
+	config, err := client.LoadConfigFromEnv()
+	if err != nil {
+		logger.Error("Error loading Operator config from env variables", zap.Error(err))
+		return
+	}
 	logger.Info("Loaded server configuration",
 		zap.String("host", config.Host),
 		zap.Int("port", config.Port),
 		zap.Bool("useTLS", config.UseTLS))
 
-	// Read JWT token from Kubernetes secret if token is not provided in environment
-	if config.Token == "" {
-		secretName := os.Getenv("AUTH_SECRET_NAME")
-		namespace := os.Getenv("POD_NAMESPACE")
-		if secretName != "" && namespace != "" {
-			jwt, err := client.ReadJWTTokenFromSecret(ctx, logger, secretName, namespace)
-			if err != nil {
-				logger.Warn("Error reading JWT token from secret", zap.Error(err))
-			} else {
-				config.Token = jwt
-			}
-		}
-	}
-
 	// Create StreamClient with the loaded configuration
-	streamClient, err := client.NewStreamClient(logger, config)
+	streamClient, err := client.NewStreamClient(ctx, logger, *config)
 	if err != nil {
 		logger.Error("Error creating stream client", zap.Error(err))
 		return
