ee47306e33445d25f8f10f68a92561b5565e7fe9
Raman Varma <ramanvarma@berkeley.edu>
2025-08-06 19:23:34 -0700

    Since the operator establishes the bidi gRPC stream once and it's longlived, using a Token source doesn't work, since it's only invoked on the first connection (operator doesn't reconnect unless it crashes); move this logic into a periodic goroutine that calls RenewClusterToken and updates the runtime secret

--- pkg/auth/token_source.go
+++ pkg/auth/token_source.go
@@ -2,18 +2,12 @@ package auth
 
 import (
 	"context"
-	"log"
-	"os"
 	"time"
 
 	serverv1 "server/api/gen/server/v1"
 
 	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/oauth2"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 )
 
 // grpcTokenSource implements oauth2.TokenSource and refreshes itself by calling
@@ -24,10 +18,6 @@ type grpcTokenSource struct {
 
 	cur *oauth2.Token
 }
-
-const (
-	RuntimeSecretName = "kestrel-operator-jwt-runtime"
-)
 
 // NewTokenSource builds a TokenSource from the *first* token obtained during onboarding.
 func NewTokenSource(
@@ -50,28 +40,7 @@ func NewTokenSource(
 }
 
 func (g *grpcTokenSource) Token() (*oauth2.Token, error) {
-	if g.cur.Valid() && time.Until(g.cur.Expiry) > 1*time.Hour {
-		return g.cur, nil
-	}
-
-	// Renew token if it expires within the next hour (proactive renewal)
-	resp, err := g.client.RenewClusterToken(
-		g.ctx,
-		&serverv1.RenewClusterTokenRequest{CurrentToken: g.cur.AccessToken})
-	if err != nil {
-		return nil, err
-	}
-	g.cur = &oauth2.Token{
-		AccessToken: resp.AccessToken,
-		TokenType:   resp.TokenType,
-		Expiry:      time.Now().Add(time.Duration(resp.ExpiresIn) * time.Second),
-	}
-
-	// Update the runtime secret with the new token
-	if err := g.updateRuntimeTokenSecret(resp.AccessToken); err != nil {
-		log.Printf("Failed to update runtime token secret: %v", err)
-	}
-
+	// Simply return the current token - renewal is handled by periodic goroutine in stream client
 	return g.cur, nil
 }
 
@@ -83,46 +52,4 @@ func parseExpiry(raw string) time.Time {
 	}
 	// pessimist fallback – refresh in 10 min
 	return time.Now().Add(10 * time.Minute)
-}
-
-// updateRuntimeTokenSecret updates the runtime secret (not the Helm-managed one)
-func (g *grpcTokenSource) updateRuntimeTokenSecret(newToken string) error {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		return err
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return err
-	}
-
-	namespace := os.Getenv("POD_NAMESPACE")
-
-	// Create or update the runtime secret
-	secret := &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      RuntimeSecretName,
-			Namespace: namespace,
-		},
-		Type: corev1.SecretTypeOpaque,
-		Data: map[string][]byte{
-			"token": []byte(newToken),
-		},
-	}
-
-	// Try to update first, create if it doesn't exist
-	_, err = clientset.CoreV1().Secrets(namespace).Update(g.ctx, secret, metav1.UpdateOptions{})
-	if err != nil {
-		// If update failed, try to create
-		_, err = clientset.CoreV1().Secrets(namespace).Create(g.ctx, secret, metav1.CreateOptions{})
-		if err != nil {
-			return err
-		}
-		log.Printf("Created runtime token secret: %s", RuntimeSecretName)
-	} else {
-		log.Printf("Successfully updated runtime token secret: %s", RuntimeSecretName)
-	}
-
-	return nil
 }
