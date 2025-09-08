4e392184ffdaa8faa77321e71253306e01fa8070
Raman Varma <ramanvarma@berkeley.edu>
2025-09-07 19:56:10 -0700

    Start the health server on operator bootup

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"log"
+	"os"
+	"strconv"
 
 	"operator/pkg/client"
 
@@ -40,8 +42,36 @@ func main() {
 	}
 	defer streamClient.Client.Close()
 
+	// Get health server port from environment variable (default to 8080)
+	healthPortStr := getEnvOrDefault("HEALTH_PORT", "8080")
+	healthPort, err := strconv.Atoi(healthPortStr)
+	if err != nil {
+		logger.Warn("Invalid HEALTH_PORT, using default 8080", zap.String("port", healthPortStr))
+		healthPort = 8080
+	}
+
+	// Create and start health server
+	healthServer := client.NewHealthServer(logger, streamClient, healthPort)
+	go func() {
+		if err := healthServer.Start(); err != nil {
+			logger.Error("Health server failed", zap.Error(err))
+		}
+	}()
+	defer healthServer.Stop()
+
+	logger.Info("Health server started", zap.Int("port", healthPort))
+
 	// Start the operator
 	if err := streamClient.StartOperator(ctx); err != nil {
 		logger.Error("Error starting operator", zap.Error(err))
 	}
+}
+
+// Helper function to get environment variable with default value
+func getEnvOrDefault(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
 }
